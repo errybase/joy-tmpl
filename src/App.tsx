5f78ffc68fae77225af152ddffc2299f25d43090
tdd28 <tdd2810@gmail.com>
2026-02-08 18:42:34 +0700

    install joy-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import './App.css';
+import { CssVarsProvider, CssBaseline } from '@mui/joy'
 
 const App = () => {
   return (
-    <div className="content">
-      <h1>Rsbuild with React</h1>
-      <p>Start building amazing things with Rsbuild.</p>
-    </div>
-  );
+    <CssVarsProvider>
+      <CssBaseline />
+    </CssVarsProvider>
+  )
 };
 
 export default App;
